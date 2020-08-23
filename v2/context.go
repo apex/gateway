5f644b75fcd525af8f8e0163c6b1c9ba19313389
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2020-08-23 16:36:19 +0300

    Add GetRequest
    It's extremely useful to have access to the original object
    
    Its QueryStringParameters are not stored to the request's URL ones with the default implementation so we need access through the original lambda request

--- v2/context.go
+++ v2/context.go
@@ -9,16 +9,25 @@ import (
 // key is the type used for any items added to the request context.
 type key int
 
-// requestContextKey is the key for the api gateway proxy `RequestContext`.
+// requestContextKey is the key for the api gateway proxy `Request`.
 const requestContextKey key = iota
 
 // RequestContext returns the APIGatewayV2HTTPRequestContext value stored in ctx.
 func RequestContext(ctx context.Context) (events.APIGatewayV2HTTPRequestContext, bool) {
-	c, ok := ctx.Value(requestContextKey).(events.APIGatewayV2HTTPRequestContext)
+	c, ok := GetRequest(ctx)
+	if ok {
+		return c.RequestContext, true
+	}
+	return events.APIGatewayV2HTTPRequestContext{}, false
+}
+
+// GetRequest returns the APIGatewayV2HTTPRequest value stored in ctx.
+func GetRequest(ctx context.Context) (events.APIGatewayV2HTTPRequest, bool) {
+	c, ok := ctx.Value(requestContextKey).(events.APIGatewayV2HTTPRequest)
 	return c, ok
 }
 
 // newContext returns a new Context with specific api gateway v2 values.
 func newContext(ctx context.Context, e events.APIGatewayV2HTTPRequest) context.Context {
-	return context.WithValue(ctx, requestContextKey, e.RequestContext)
+	return context.WithValue(ctx, requestContextKey, e)
 }
